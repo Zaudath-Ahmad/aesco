a5930f918d485cb0ba4450d5b52ab5709787c0ec
Zaudath <144656987+Zaudath-Ahmad@users.noreply.github.com>
2024-01-31 23:35:38 +0700

    Add files via upload

---
+++ PORTOFOLIO 2/content.js
@@ -0,0 +1,67 @@
+var contactObject = document.getElementById('navContact');
+var contactSlide = false;
+
+function contact() {
+    if (contactSlide === false) {
+        contactSlide = true;
+        contactObject.style.left = "60vw";
+    } else {
+        contactSlide = false;
+        contactObject.style.left = "100vw";
+    }
+}
+
+function defaultClickContact() {
+    contactSlide = false;
+    contactObject.style.left = "100vw";
+}
+
+function btnScroll(param) {
+    param === "product" ? location.href = "#productDiv" : location.href = "#galeryDiv"
+}
+
+window.addEventListener("scroll", function() {
+    var element = document.querySelector(".navbar-container");
+
+    if (window.scrollY > 890) {
+        element.style.color = "white"
+    } else {
+        element.style.color = "black"
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('formRequest').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        submitRequest();
+    });
+});
+
+function submitRequest() {
+    alert("Request Accepted")
+}
+
+var coffeeBtn = true;
+var bakeryBtn = true;
+var coffeeBlock = document.getElementById('coffeeBlock');
+var bakeryBlock = document.getElementById('bakeryBlock')
+
+function coffee() {
+    if (coffeeBtn == true) {
+        coffeeBtn = false;
+        coffeeBlock.style.left = "0vw";
+    } else {
+        coffeeBtn = true;
+        coffeeBlock.style.left = "100vw";
+    }
+}
+
+function bakery() {
+    if (bakeryBtn == true) {
+        bakeryBtn = false;
+        bakeryBlock.style.left = "0vw";
+    } else {
+        bakeryBtn = true;
+        bakeryBlock.style.left = "100vw";
+    }
+}
